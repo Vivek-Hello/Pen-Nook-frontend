f2e40c63708620c636ffa2d92ad8d972e55a7532
Vivek-Hello <vickyvivek391@gmail.com>
2025-02-09 12:06:05 +0530

    add in connection

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true // Crucial for cookies
 });
 
