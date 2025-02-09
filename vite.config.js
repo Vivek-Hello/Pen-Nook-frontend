2e80d38453e51b9e30bdb3f42e7d3424053185b2
Vivek-Hello <vickyvivek391@gmail.com>
2025-02-09 18:05:46 +0530

    changin the url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Your backend URL
+        target: 'https://pen-nook-backend.onrender.com', // Your backend URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
